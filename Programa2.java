f519fffb9931cf5945e274ae813d5ab855dcecbf
TristanSantiago1 <132310054+TristanSantiago1@users.noreply.github.com>
2024-03-16 19:46:34 -0600

    Add files via upload

---
+++ Programa2.java
@@ -0,0 +1,30 @@
+public class Programa2{
+    private static Object hora = new Object();
+
+    private static class JuntaThread implements Runnable{
+        public JuntaThread(){
+            hora = 9;
+        }
+        @Override
+        public void run(){
+            realizarJunta();
+        }
+
+        public static  void realizarJunta(){
+            synchronized(hora){
+                System.out.println(Thread.currentThread().getName() + " Junta inciada a la " + hora);
+                hora = (int) hora +1;
+                System.out.println(Thread.currentThread().getName() + " Junta terminada a la " + hora);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        for(int i=0; i< 10; i++){
+            Thread hilo1 = new Thread(new JuntaThread(), "Junta- " + (i+1));
+            hilo1.start();
+        }
+    }
+
+
+}
